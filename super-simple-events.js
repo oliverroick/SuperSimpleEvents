84db8676ec39f2e7710eecc9b232e1c7d84f34a0
Oliver Roick <oliver.roick@gmail.com>
2014-06-09 16:23:55 +0100

    Renamed methods

--- super-simple-events.js
+++ super-simple-events.js
@@ -17,7 +17,7 @@ var EventEmitter = (function () {
      * @param  {String} ev The event identifier.
      * @param  {Function} callback The callback function.
      */
-    function register(ev, callback) {
+    function registerListener(ev, callback) {
         if (!this.events[ev]) {this.events[ev] = []; }
         this.events[ev].push(callback);
     }
@@ -27,7 +27,7 @@ var EventEmitter = (function () {
      * @param  {String} ev The event identifier
      * @param  {Object} eventObj An optional object giving information on the state of the event
      */
-    function emit(ev, eventObj) {
+    function emitEvent(ev, eventObj) {
         if (this.events[ev]) {
             for (var i = 0, len = this.events[ev].length; i < len; i++) {
                 this.events[ev][i](eventObj);
