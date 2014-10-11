e84a8dbd95aeee3af7813ec55dfd7d274a82b820
Oliver Roick <oliver.roick@gmail.com>
2014-10-11 23:09:15 +0100

    Jasmine grunt tests

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+
+    // 1. All configuration goes here
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        jasmine: {
+            components: {
+              src: [
+              'src/*js'
+              ],
+              options: {
+                specs: 'spec/*.js',
+                keepRunner : true
+                }
+            }
+        }
+    });
+
+    // 3. Where we tell Grunt we plan to use this plug-in.
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+    grunt.registerTask('travis', ['jasmine']);
+};
